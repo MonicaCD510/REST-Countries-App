c8679a59bf25c724debcaed6ba8e0f3dce045539
Monica Davila <monica.davila510@gmail.com>
2026-04-10 20:15:31 -0500

    render all country names from API data

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,17 +1,19 @@
 const container = document.getElementById('countries-container');
 
-console.log("js is running");
-
 fetch("https://restcountries.com/v3.1/all?fields=name")
   .then(response => response.json())
   .then(data => {
     console.log(data);
 
-    if (data && data.length > 0) {
-      container.innerHTML = `<h2>${data[0].name.common}</h2>`;
-    } else {
-      container.innerHTML = "<p>No data found</p>";
-    }
+    container.innerHTML = "";
+
+    data.forEach(country => {
+      container.innerHTML += `
+        <div>
+          <h2>${country.name.common}</h2>
+        </div>
+      `;
+    });
   })
   .catch(error => {
     console.log(error);
