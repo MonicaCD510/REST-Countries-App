0649f6c13500ef11e6b20eeaa38b415f624e0d91
Monica Davila <monica.davila510@gmail.com>
2026-04-11 12:42:27 -0500

    add country flags to display

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,6 +1,5 @@
 const container = document.getElementById('countries-container');
-
-fetch("https://restcountries.com/v3.1/all?fields=name")
+fetch("https://restcountries.com/v3.1/all?fields=name,flags")
   .then(response => response.json())
   .then(data => {
     console.log(data);
@@ -10,6 +9,7 @@ fetch("https://restcountries.com/v3.1/all?fields=name")
     data.forEach(country => {
       container.innerHTML += `
         <div>
+         <img src="${country.flags.png}" width="100">
           <h2>${country.name.common}</h2>
         </div>
       `;
