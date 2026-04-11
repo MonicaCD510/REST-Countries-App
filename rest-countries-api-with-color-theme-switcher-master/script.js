16ef48766ebe4447d19f435b6dcab0e7726c5721
Monica Davila <monica.davila510@gmail.com>
2026-04-10 20:04:12 -0500

    display first country from API data

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,9 +1,19 @@
-fetch("https://restcountries.com/v3.1/all")
-  .then(res => res.json()) // convert response to json
+const container = document.getElementById('countries-container');
+
+console.log("js is running");
+
+fetch("https://restcountries.com/v3.1/all?fields=name")
+  .then(response => response.json())
   .then(data => {
     console.log(data);
+
+    if (data && data.length > 0) {
+      container.innerHTML = `<h2>${data[0].name.common}</h2>`;
+    } else {
+      container.innerHTML = "<p>No data found</p>";
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    container.innerHTML = "<p>Error loading data</p>";
   });
-
-  const container = document.getElementById("countries-container");
-console.log(container);
-
