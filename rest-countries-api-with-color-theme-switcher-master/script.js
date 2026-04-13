bc2eba78fd2b0724be0be5386e2761e8ed545f16
Monica Davila <monica.davila510@gmail.com>
2026-04-12 22:47:03 -0500

    use displayCountries function to render countries

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,35 +1,25 @@
-function displayCountries(data) {
-  displayCountries(data);
+const container = document.getElementById('countries-container');
 
-  {
+function displayCountries(data) {
+  container.innerHTML = "";
+
+  data.forEach(country => {
     container.innerHTML += `
       <div>
         <img src="${country.flags.png}" width="100">
         <h2>${country.name.common}</h2>
         <p>Population: ${country.population}</p>
         <p>Region: ${country.region}</p>
-    </div>
+      </div>
     `;
-  };
+  });
 }
-const container = document.getElementById('countries-container');
+
 fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,region")
   .then(response => response.json())
   .then(data => {
     console.log(data);
-
-    container.innerHTML = "";
-
-    data.forEach(country => {
-      container.innerHTML += `
-        <div>
-         <img src="${country.flags.png}" width="100">
-          <h2>${country.name.common}</h2>
-          <p>Population: ${country.population}</p>
-          <p>Region: ${country.region}</p>
-        </div>
-      `;
-    });
+    displayCountries(data); // ✅ this is where it belongs
   })
   .catch(error => {
     console.log(error);
