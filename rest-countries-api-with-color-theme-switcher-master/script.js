c12b58e7068e9891ab26a4a88857f6effc90cdff
Monica Davila <monica.davila510@gmail.com>
2026-04-12 22:35:06 -0500

    add region to country display

--- rest-countries-api-with-color-theme-switcher-master/script.js
+++ rest-countries-api-with-color-theme-switcher-master/script.js
@@ -1,15 +1,16 @@
 function displayCountries(data) {
-  container.innerHTML = "";
+  displayCountries(data);
 
-  data.forEach(country => {
+  {
     container.innerHTML += `
       <div>
         <img src="${country.flags.png}" width="100">
         <h2>${country.name.common}</h2>
         <p>Population: ${country.population}</p>
-      </div>
+        <p>Region: ${country.region}</p>
+    </div>
     `;
-  });
+  };
 }
 const container = document.getElementById('countries-container');
 fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,region")
